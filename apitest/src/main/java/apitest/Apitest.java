bea66fc4e65c0186ab7d75a95a1885c74c0af5d9
Nick Lonsway <nlonz@tamu.edu>
2016-02-29 08:50:06 -0600

    Updated apitest to get the last week of weather

--- apitest/src/main/java/apitest/Apitest.java
+++ apitest/src/main/java/apitest/Apitest.java
@@ -4,21 +4,51 @@ package apitest;
 
 import org.springframework.web.client.RestTemplate;
 import org.json.JSONObject;
+import org.json.JSONException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class Apitest{
-	public static void main(String args[]) throws Exception{
-		RestTemplate restTemplate = new RestTemplate();
+	public static String unixTimeToDate(long unix){
+		Date date = new Date(unix);
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT-6"));
+		String formattedDate = sdf.format(date);
+		return formattedDate;
+	}
+	
+	public static JSONObject getCurrentWeather(String date) throws JSONException{
+		//Creates the API request string
+		String APIKey = "c62d91cb9c000638716e55cbc478330f"; //Replace this if we run out of requests on an account
+		String latitude = "30.6014";
+		String longitude = "-96.3144"; //College Station, TX
+		String request = "https://api.forecast.io/forecast/" + APIKey + "/" + latitude + "," +
+						longitude + "," + date;
 		
 		//GETs weather data as a String and converts it to a JSONObject called weatherObj. 
 		//Because the temperature is contained inside another JSONObject within weatherObj, we must
 		//get the "currently" report and parse that to retrieve temperature.
-		String daysWeather = restTemplate.getForObject("https://api.forecast.io/forecast/c62d91cb9c000638716e55cbc478330f/30.6014,-96.3144,2015-02-25T12:00:00",String.class);
-		JSONObject weatherObj = new JSONObject(daysWeather);
-		String currently = (weatherObj.get("currently").toString());
-		JSONObject currentObj = new JSONObject(currently);
-		
-		System.out.println(weatherObj.get("currently"));
-		System.out.println(currentObj.get("temperature"));
-		System.out.println(weatherObj);
+		RestTemplate restTemplate = new RestTemplate();
+		String daysWeather = restTemplate.getForObject(request,String.class);
+		JSONObject weatherObj = new JSONObject(daysWeather); 
+		String currently = weatherObj.get("currently").toString();
+		JSONObject currentlyObj = new JSONObject(currently);
+		return currentlyObj;
+	}
+	
+	public static void main(String args[]) throws JSONException{
+		//Currently retrieves the last week of temperatures
+		//http://stackoverflow.com/questions/17432735/convert-unix-time-stamp-to-date-in-java
+		long currentTime = System.currentTimeMillis();
+		for(int index=0; index<7; index++){
+			String requestTime = unixTimeToDate(currentTime);
+			JSONObject currentlyObj = getCurrentWeather(requestTime);
+			System.out.print("Date: ");
+			System.out.print(requestTime);
+			System.out.print("\tTemperature: ");
+			System.out.println(currentlyObj.get("temperature") + "\n");
+			currentTime-=86400000; //86,400,000 milliseconds in a day
+		}
 	}
 }
