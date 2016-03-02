5d4ad81ac8c4b48180e952a53b9561cbabfc3df4
Nick Lonsway <nlonz@tamu.edu>
2016-03-01 18:26:20 -0600

    Added JSON jar and did some DRYing

--- apitest/src/main/java/apitest/Apitest.java
+++ apitest/src/main/java/apitest/Apitest.java
@@ -24,11 +24,14 @@ public class Apitest{
 		return formattedDate;
 	}
 	
-	public static String makeAPIRequest() throws JSONException{
+	//Case for future weather data where no date is required
+	public static String makeAPIRequest(){
 		return makeAPIRequest("");
 	}
 	
-	public static String makeAPIRequest(String date) throws JSONException{
+	//Case for past weather data where a date is required
+	public static String makeAPIRequest(String date){
+		//Creates the API request string
 		String APIKey = "c62d91cb9c000638716e55cbc478330f"; //Replace this if we run out of requests on an account
 		String latitude = "30.6014";
 		String longitude = "-96.3144"; //College Station, TX
@@ -39,21 +42,43 @@ public class Apitest{
 		return request;
 	}
 	
-	public static JSONObject getPastFullDayWeather(String date) throws JSONException{
-		//Creates the API request string
-		String request = makeAPIRequest(date);
-		
-		//GETs weather data as a String and converts it to a JSONObject 
+	public static JSONObject makeAPICall(String request) throws JSONException{
+		//I used the "Consuming a RESTful Web Service" found here: https://spring.io/guides/gs/consuming-rest/
 		RestTemplate restTemplate = new RestTemplate();
 		String daysWeather = restTemplate.getForObject(request,String.class);
 		JSONObject weatherObj = new JSONObject(daysWeather);
+		return weatherObj;
+	}
+	
+	public static JSONObject getPastFullDayWeather(String date) throws JSONException{
+		String request = makeAPIRequest(date);
+		JSONObject weatherObj = makeAPICall(request);
 		
-		//We don't need the current or hourly report, just data for the whole day.
+		//We don't need the "current" or hourly report, just data for the whole day
 		//This data is presented as a JSONObject that contains a JSONArray with one element
+		//With that element being the JSONObject containing the daily report
 		JSONObject fakeDailyObj = weatherObj.getJSONObject("daily");
 		JSONArray arr = (JSONArray) fakeDailyObj.get("data");
 		JSONObject dailyObj = (JSONObject) arr.get(0);
 		return dailyObj;
+	}
+	
+	public static void displayWeatherData(JSONObject obj) throws JSONException{
+		System.out.print("Date: ");
+		System.out.println(unixTimeToDate((obj.getLong("time")*1000)));
+		System.out.print("High Temperature: ");
+		System.out.println(obj.get("temperatureMax"));
+		System.out.print("Low Temperature: ");
+		System.out.println(obj.get("temperatureMin"));
+		System.out.print("Precipitation % Chance: ");
+		System.out.println((int)((obj.getDouble("precipProbability"))*100) + "%");
+		System.out.print("Outlook: ");
+		System.out.println(obj.get("summary"));
+		System.out.print("Wind Speed: ");
+		System.out.println(obj.get("windSpeed"));
+		System.out.print("Wind Direction (0 = N, 90 = E, etc): ");
+		System.out.println(obj.get("windBearing"));		
+		System.out.println();
 	}
 	
 	public static void lastWeeksForecast() throws JSONException{
@@ -61,53 +86,22 @@ public class Apitest{
 		for(int index = 0; index < 7; index++){
 			String requestTime = unixTimeToDate(currentTime);
 			JSONObject dailyObj = getPastFullDayWeather(requestTime);
-			
-			System.out.print("Date: ");
-			System.out.println(requestTime);
-			System.out.print("High Temperature: ");
-			System.out.println(dailyObj.get("temperatureMax"));
-			System.out.print("Low Temperature: ");
-			System.out.println(dailyObj.get("temperatureMin"));
-			System.out.print("Precipitation % Chance: ");
-			System.out.println((int)((dailyObj.getDouble("precipProbability"))*100) + "%");
-			System.out.print("Outlook: ");
-			System.out.println(dailyObj.get("summary"));
-			System.out.print("Wind Speed: ");
-			System.out.println(dailyObj.get("windSpeed"));
-			System.out.print("Wind Direction (0 = N, 90 = E, etc): ");
-			System.out.println(dailyObj.get("windBearing"));
-			
-			System.out.println();
+			displayWeatherData(dailyObj);
 			currentTime-=86400000; //86,400,000 milliseconds in a day
 		}
 	}
 	
 	public static void nextWeeksForecast() throws JSONException{
 		String request = makeAPIRequest();
+		JSONObject weatherObj = makeAPICall(request);
 		
-		RestTemplate restTemplate = new RestTemplate();
-		String forecast = restTemplate.getForObject(request, String.class);
-		JSONObject weatherObj = new JSONObject(forecast);
-		
+		//Like the past data, each day's data is a JSONObject inside a JSONArray inside a JSONObject
+		//In this case, there are 8 objects inside the array, with each one representing a day
 		JSONObject dailyForecastObj = weatherObj.getJSONObject("daily");
 		JSONArray days = (JSONArray) dailyForecastObj.get("data");
 		for(int index = 0; index < 8; ++index){
 			JSONObject dayObj = (JSONObject) days.get(index);
-			System.out.print("Date: ");
-			System.out.println(unixTimeToDate((dayObj.getLong("time")*1000)));
-			System.out.print("High Temperature: ");
-			System.out.println(dayObj.get("temperatureMax"));
-			System.out.print("Low Temperature: ");
-			System.out.println(dayObj.get("temperatureMin"));
-			System.out.print("Precipitation % Chance: ");
-			System.out.println((int)((dayObj.getDouble("precipProbability"))*100) + "%");
-			System.out.print("Outlook: ");
-			System.out.println(dayObj.get("summary"));
-			System.out.print("Wind Speed: ");
-			System.out.println(dayObj.get("windSpeed"));
-			System.out.print("Wind Direction (0 = N, 90 = E, etc): ");
-			System.out.println(dayObj.get("windBearing"));
-			System.out.println();
+			displayWeatherData(dayObj);
 		}
 	}
 	
