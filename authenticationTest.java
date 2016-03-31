e9b23c58f73e6b8368174cfade3650e8827d60a3
cqr1 <pwr-181.lift@tamu.edu>
2016-03-31 09:24:49 -0500

    Update authenticationTest.java
    tested JUNIT file. tests all passed. ready for integration.

--- authenticationTest.java
+++ authenticationTest.java
@@ -1,51 +1,55 @@
-package api;
+package test;
 
-import org.json.JSONObject;
 import static org.junit.Assert.*;
-import org.json.JSONException;
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
 import org.junit.*;
 
 // import hashing utils
 // going to do this later
 
 
-public class authenticationTest{
+public class authTest{
   
 	String realHash;
 	
 	String realUsername = "AuthUser";
 	String realPassword = "teamDefault482";
 	
+	authentication AUTH = new authentication();
+	
 	@Before
-	public void setUpRealHash(){
-	  realHash = HMAC(realUsername, realPassword);
+	public void setUpRealHash() throws InvalidKeyException, NoSuchAlgorithmException{
+	  realHash = AUTH.HMAC(realUsername, realPassword);
 	}
 	
-	@test
+	@Test
 	public void testValidCreds() throws Exception{
-	  String test1 = HMAC(realUsername, realPassword);
+	  String test1 = AUTH.HMAC(realUsername, realPassword);
 	  assertEquals(realHash, test1);
 	}
 	
-	@test
+	@Test
 	public void testValidPassword() throws Exception{
-	  String test2 = HMAC(realUsername, "TeamDefault482");
+	  String test2 = AUTH.HMAC(realUsername, "TeamDefault482");
 	  //assertEquals(realHash, test2);
 	  assertFalse(realHash.equals(test2));
 	}
 	
-	@test
+	@Test
 	public void testValidUsername() throws Exception{
-	  String test3 = HMAC("authUser", realPassword);
+	  String test3 = AUTH.HMAC("authUser", realPassword);
 	  //assertEquals(realHash, test3);
 	  assertFalse(realHash.equals(test3));
 	}
 	
-	@test
+	@Test
 	public void testValidCreds2() throws Exception{
-	  String test4 = HMAC("AuthUser", "teamDefault482");
+	  String test4 = AUTH.HMAC("AuthUser", "teamDefault482");
 	  //assertEquals(realHash, test4);
-	  assertFalse(realHash.equals(test4));
+	  assertEquals(realHash, test4);
 	}
 	
 }
